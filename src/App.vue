bb28428399862559a534ef09c61c3138f7680ab2
1393925530 <zzc1393925530@gmail.com>
2019-06-22 23:51:14 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="test">{{ text }}</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      text: 'abc'
+    }
+  }
+}
+</script>
+
+<style>
+#test {
+  color: red
+}
+</style>
